c986508d8b1760426af35485a5ea509d588273b4
Dudnyk Vitalii <vetal19955555@gmail.com>
2022-04-29 22:50:14 +0300

    use cases to fetching photos implemented

--- app/src/main/java/com/dudnyk/framework/flickrgallery/domain/use_case/getPhotosByGroupIdUseCase.kt
+++ app/src/main/java/com/dudnyk/framework/flickrgallery/domain/use_case/getPhotosByGroupIdUseCase.kt
@@ -1,12 +1,37 @@
 package com.dudnyk.framework.flickrgallery.domain.use_case
 
+import com.dudnyk.framework.flickrgallery.common.Resource
+import com.dudnyk.framework.flickrgallery.domain.model.Photo
 import com.dudnyk.framework.flickrgallery.domain.repository.Repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 class getPhotosByGroupIdUseCase(
     private val repository: Repository
 ) {
 
-    suspend operator fun invoke(queryKey: String) {
-
+    suspend operator fun invoke(groupId: String): Flow<Resource<List<Photo>>> = flow {
+        try {
+            emit(Resource.Loading(null))
+            val listOfPhotos = repository.getPhotosByGroupId(groupId = groupId)
+            emit(Resource.Success(listOfPhotos))
+        } catch (e: Exception) {
+            when (e) {
+                is HttpException, is UnknownHostException -> emit(
+                    Resource.NetworkError(
+                        message = e.localizedMessage
+                            ?: "Couldn't reach server. Check your internet connection"
+                    )
+                )
+                else -> emit(
+                    Resource.Error(
+                        message = e.localizedMessage
+                            ?: "An unexpected error occurred"
+                    )
+                )
+            }
+        }
     }
 }
